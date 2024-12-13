d1b0debed35f569cb4318d7177b49f4dfc830ca5
SahilRepale <118911800+SahilRepale@users.noreply.github.com>
2024-12-13 13:20:18 +0530

    Update sahil.py

--- sahil.py
+++ sahil.py
@@ -385,7 +385,7 @@ def save_ip_port(message):
         bot.send_message(chat_id, "Invalid format. Please enter a valid IP and port.")
 
 # Function to start the attack
-@bot.message_handler(func=lambda message: message.text == 'Start Attack 🚀')
+@bot.message_handler(func=lambda message: message.text == 'Start Attack')
 def handle_start_attack(message):
     try:
         user_id = message.from_user.id
